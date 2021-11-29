400475ef297c2b2cbd9f572e7cb8627c83ea432b
Keith O'Hara <keith.ohara@gmail.com>
2021-11-29 09:32:27 -0500

    updated sketch to draw based on mouse

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
+  createCanvas(500, 500);
+  background(196);
   // put setup code here
 }
 
 function draw() {
   // put drawing code here
+  stroke(24);
+  strokeWeight(2);
+  line(pmouseX, pmouseY, mouseX, mouseY);
+  line(width-pmouseX, pmouseY, width-mouseX, mouseY);
 }
